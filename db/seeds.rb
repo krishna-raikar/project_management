ec19b269008b51fb84b5cc4bf7a07eb01aba718a
krishna <krishna.raikar@kvpcorp.com>
2016-03-04 16:30:18 +0530

    image loading fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,11 @@ Permission.create(role_id:'2',modelname:'issue',per_list:'["show", "edit", "crea
 Permission.create(role_id:'2',modelname:'project',per_list:'["show", "edit"]')
 Permission.create(role_id:'3',modelname:'user',per_list:'["show"]')
 Permission.create(role_id:'3',modelname:'project user',per_list:'["show", "edit", "create", "destroy"]')
+
+
+User.create(firstname:'Krishna',lastname:'Raikar',phone:'',role_id:'2',email:'krishna.raikar@kvpcorp.com',password:'123456789')
+User.create(firstname:'Krishna',lastname:'Raikar',phone:'',role_id:'2',email:'krishna.raikar1993@gmail.com',password:'123456789')
+User.create(firstname:'varun',lastname:'nagpal',phone:'9633245155',role_id:'3',email:'varun.nagpal@gmail.com',password:'123456789')
+User.create(firstname:'adminkvr',lastname:'raikar',phone:'9655152455',role_id:'1',email:'admin@gmail.com',password:'admin123')
+User.create(firstname:'nataraja',lastname:'sheikh',phone:'9652415233',role_id:'2',email:'nataraj.sheikh@gmail.com',password:'123456789')
+User.create(firstname:'rajendra',lastname:'kulkarni',phone:'9652441524',role_id:'2',email:'rajendra.kulkarni@gmail.com',password:'123456789')
